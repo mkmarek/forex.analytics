a0365ef3acd43d4cbae298b78ed5719a6c5b9432
Marek Magdziak <marek.magdziak@hotmail.cz>
2016-03-06 21:29:45 +0100

    npm fix

---
+++ index.js
@@ -0,0 +1,6 @@
+var analytics = require('./build/Release/analytics.node');
+var analytics2 = _interopRequireDefault(analytics);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = analytics2.default;
